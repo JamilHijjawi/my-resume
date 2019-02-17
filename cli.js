c076d05ae43e04f3265c5fec76a5c8c14c7dd63c
Jamil Hijjawi <j.hijjawi@gmail.com>
2019-02-17 12:32:00 +0200

    Enhance content

--- cli.js
+++ cli.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
 
+const { render } = require('prettyjson');
 
-console.log(`Hey I'm Jamil`);
+const me = {
+    name: 'Jamil Hijjawi',
+    title: 'Sotfware engineer - Front end developer',
+    github: 'https://github.com/JamilHijjawi',
+    linkedIn: 'https://www.linkedin.com/in/jamilhijjawi/',
+    email: 'j.hijjawi@gmail.com',
+    skills: `Javascript, Nodejs, Express, Angular, React, Ionic, Cordova, Stencil, Polymer, Scrum, CI/CD, TDD, Mysql, Gatsby, GraphQL, OOP, Functional Programming, Reactive Programming, Web Socket, Web development, SCSS, CSS, HTML`
+};
+
+const renderOpts = {
+    dashColor: 'cyan',
+    keysColor: 'blue',
+    stringColor: 'white'
+};
+
+console.log(render(me, renderOpts));
