3645e8547db9c9fee49ec6bb2af4c5379d082916
Jamil Hijjawi <j.hijjawi@gmail.com>
2019-02-17 13:52:23 +0200

    Add stackoverflow url

--- cli.js
+++ cli.js
@@ -7,6 +7,7 @@ const me = {
     title: 'Sotfware engineer - Front end developer',
     github: 'https://github.com/JamilHijjawi',
     linkedIn: 'https://www.linkedin.com/in/jamilhijjawi/',
+    stackOverflow: 'https://stackoverflow.com/users/3243642/jamil-hijjawi',
     email: 'j.hijjawi@gmail.com',
     skills: `Javascript, Nodejs, Express, Angular, React, Ionic, Cordova, Stencil, Polymer, Scrum, CI/CD, TDD, Mysql, Gatsby, GraphQL, OOP, Functional Programming, Reactive Programming, Web Socket, Web development, SCSS, CSS, HTML`
 };
